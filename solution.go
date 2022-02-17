146d5778b5330e54c4da71ed3925d76c443d6873
trauma <futuratrauma@protonmail.com>
2022-02-17 18:44:07 +0300

    2

--- solution.go
+++ solution.go
@@ -4,6 +4,8 @@ import "math"
 
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
+type Figure int
+
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
 // it's like:
 // CalcSquare(10.0, SidesTriangle)
